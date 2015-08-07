a019b0a61a58d27fb9a4bb1659704484e2483eaa
Thiago Paim <thiagopain@gmail.com>
2015-08-07 10:53:58 -0300

    Update gulpfile.js
    Ajustes no código e adição do  gulp-uglify.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
-var 	gulp 		 = require('gulp'),
-	concat 		 = require('gulp-concat'),
-	minifyCSS 	 = require('gulp-minify-css'),
-	autoprefixer 	 = require('gulp-autoprefixer'),
-	rename 		 = require('gulp-rename'),
-	watch		 = require('gulp-watch'),
-	browserSync  	 = require('browser-sync');
+var gulp					= require('gulp'),
+	 concat					= require('gulp-concat'),
+	 minifyCSS			= require('gulp-minify-css'),
+	 uglifyJS	 			= require('gulp-uglify'),
+	 autoprefixer 	= require('gulp-autoprefixer'),
+	 rename 		 		= require('gulp-rename'),
+	 watch		 			= require('gulp-watch'),
+	 browserSync  	= require('browser-sync');
 
-// minifica e concatena css
+// minifica e concatena folha de estilos
 gulp.task('styles', function(){
 	gulp.src('./assets/css/*.css')
 		.pipe(minifyCSS({
@@ -16,18 +17,28 @@ gulp.task('styles', function(){
  		.pipe(gulp.dest('dist/css'))
 });
 
+// minifica e concatena javascript
+gulp.task('js', function(){
+	gulp.src('./assets/js/*.js')
+		.pipe(uglifyJS())
+ 		.pipe(concat('scripts.js'))
+ 		.pipe(gulp.dest('dist/js'))
+});
+
 // verifica arquivos
 gulp.task('watch', function() {
     gulp.watch('./assets/css/*.css', ['styles']);
+    gulp.watch('./assets/js/*.js', ['js']);
 });
 
-// atualiza browser
+// browser releaod
 gulp.task('browser-sync', function() {
 	browserSync({
-		proxy: '[COLOCAR O IP E A PASTA DO WORDPRESS AQUI]'
+		proxy: "[COLOCAR O IP E A PASTA DO WORDPRESS AQUI]"
 	});
 });
 
 gulp.task('default', ['browser-sync'], function(){
 	gulp.watch('./assets/css/*.css', ['styles', browserSync.reload]);
+	gulp.watch('./assets/js/*.js', ['js', browserSync.reload]);
 });
