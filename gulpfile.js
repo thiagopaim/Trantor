bbd1c287df5b24c33f0dc2084b6925b20c465281
Thiago Paim <thiagopain@gmail.com>
2015-08-11 15:46:16 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var gulp					= require('gulp'),
-		concat				= require('gulp-concat'),
-		minifyCSS			= require('gulp-minify-css'),
-		uglifyJS			= require('gulp-uglify'),
+var gulp						= require('gulp'),
+		concat					= require('gulp-concat'),
+		minifyCSS				= require('gulp-minify-css'),
+		uglifyJS				= require('gulp-uglify'),
 		autoprefixer		= require('gulp-autoprefixer'),
-		rename				= require('gulp-rename'),
-		watch				= require('gulp-watch'),
+		rename					= require('gulp-rename'),
+		watch						= require('gulp-watch'),
 		browserSync			= require('browser-sync');
 
 // minifica e concatena folha de estilos
